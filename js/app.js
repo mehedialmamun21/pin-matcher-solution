9653648f015f749bf005ce71e568c53e35e992b1
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-02-19 01:07:33 +0600

    all major functionality done with js

--- js/app.js
+++ js/app.js
@@ -1,19 +1,57 @@
-function getPin() {
+function displayPin() {
     const pin = Math.round(Math.random() * 10000);
-    const pinString = (pin + '');
-    if (pinString.length == 4) {
-        return pin;
+    const pinLength = (pin + '').length;
+    if (pinLength == 4) {
+        const showPin = document.getElementById('show-pin');
+        showPin.value = pin;
     } else {
-        // console.log('got 3 digit & calling again', pin)
-        return getPin();
+        displayPin();
     }
-
 }
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const inputKey = document.getElementById('input-key');
+    const x = inputKey.value;
+    if (isNaN(number)) {
+        if (number == 'C') {
+            inputKey.value = '';
+        }
+        if (number == '<') {
+            inputKey.value = x.substring(0, x.length - 1);
+        }
+    } else {
+        inputKey.value = inputKey.value + number;
+    }
+})
+function submitClickFunction() {
+    const showPin = document.getElementById('show-pin').value;
+    const inputKey = document.getElementById('input-key').value;
+    const successMessage = document.getElementById('success-message')
+    const failMessage = document.getElementById('fail-message');
+    if (showPin == inputKey) {
+        successMessage.style.display = 'block';
+        failMessage.style.display = 'none';
+    }
+    else {
 
-function generatePin() {
-    const pin = getPin()
-    // console.log(pin);
-    const displayPin = document.getElementById('display-pin');
-    displayPin.value = pin;
+        failMessage.style.display = 'block';
+        successMessage.style.display = 'none';
 
+        const tryLeftId = document.getElementById('try-left-id');
+        tryLeftId.style.display = 'block';
+
+        if (showPin != inputKey) {
+            const tryLeft = document.getElementById('try-left');
+            tryLeftText = tryLeft.innerText;
+            const tryLeftValue = parseInt(tryLeftText);
+            tryLeft.innerText = tryLeftValue - 1;
+            if (tryLeft.innerText == 0) {
+                tryLeft.innerText = 'try after 5 minutes!';
+                const tryLeftText = document.getElementById('try-left-text');
+                tryLeftText.innerText = '';
+            }
+        }
+
+
+    }
 }
